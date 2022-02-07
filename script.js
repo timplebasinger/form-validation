54dba94dfa338397cc05c1dc792a756bd8fa9037
Timple Basinger <tbasinger@protonmail.com>
2022-02-07 16:25:24 -0600

    Restructure validation for ease of scaling

--- script.js
+++ script.js
@@ -7,52 +7,76 @@ const password2 = document.getElementById("password2");
 // Show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control error'
-  const small = formControl.querySelector('small');
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
   small.innerText = message;
 }
 
 //Show sucess outline
-function showSuccess(input)  {
-const formControl = input.parentElement;
-  formControl.className = 'form-control success';
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
 }
 
 // Check email is valid
 function isValidEmail(email) {
-  const re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//Get field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 //Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username)
-  }
-
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid')
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password)
-  }
-
-  if (password2.value === '') {
-    showError(password2, 'Password 2 is required');
-  } else {
-    showSuccess(password2)
-  }
-
+  checkRequired([username, email, password, password2]);
 });
+
+
+
+// //Event listeners *****Basic Form Validation*****
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   if (username.value === '') {
+//     showError(username, 'Username is required');
+//   } else {
+//     showSuccess(username)
+//   }
+
+//   if (email.value === '') {
+//     showError(email, 'Email is required');
+//   } else if(!isValidEmail(email.value)) {
+//     showError(email, 'Email is not valid')
+//   } else {
+//     showSuccess(email);
+//   }
+
+//   if (password.value === '') {
+//     showError(password, 'Password is required');
+//   } else {
+//     showSuccess(password)
+//   }
+
+//   if (password2.value === '') {
+//     showError(password2, 'Password 2 is required');
+//   } else {
+//     showSuccess(password2)
+//   }
+
+// });
